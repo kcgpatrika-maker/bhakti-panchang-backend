960a785b1963d56eb2c5fdde2a3700f70daf1828
kcgpatrika-maker <kcg.patrika@gmail.com>
2025-12-29 14:50:26 +0530

    Create tithiCalendar.js

---
+++ data/tithiCalendar.js
@@ -0,0 +1,13 @@
+export const tithiCalendar = {
+  "2025-12-27": {
+    masa: "पौष",
+    paksha: "शुक्ल पक्ष",
+    tithi: "सप्तमी"
+  },
+
+  "2025-12-29": {
+    masa: "पौष",
+    paksha: "शुक्ल पक्ष",
+    tithi: "नवमी"
+  }
+};
