576f09feb0a1d18d2445314f0f64b5f4eea49813
kcgpatrika-maker <kcg.patrika@gmail.com>
2026-01-03 13:54:37 +0530

    Create tithiTable.js

---
+++ data/tithiTable.js
@@ -0,0 +1,20 @@
+// data/tithiTable.js
+// Trusted manual tithi table (Drik Panchang based)
+
+export const TITHI_TABLE = {
+  "2026-01-02": {
+    masa: "पौष",
+    tithi: "चतुर्दशी",
+    paksha: "शुक्ल पक्ष"
+  },
+  "2026-01-03": {
+    masa: "पौष",
+    tithi: "पूर्णिमा",
+    paksha: "शुक्ल पक्ष"
+  },
+  "2026-01-04": {
+    masa: "पौष",
+    tithi: "प्रतिपदा",
+    paksha: "कृष्ण पक्ष"
+  }
+};
