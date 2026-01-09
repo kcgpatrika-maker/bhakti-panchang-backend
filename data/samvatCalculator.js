93ed828ec3e0631bef299ad528f7e92461605afa
kcgpatrika-maker <kcg.patrika@gmail.com>
2026-01-09 18:48:02 +0530

    Update samvatCalculator.js

--- data/samvatCalculator.js
+++ data/samvatCalculator.js
@@ -1,7 +1,7 @@
 export function getSamvat(date) {
   const year = date.getFullYear();
   return {
-    vikram_samvat: year + 56,
+    vikram_samvat: year + 56.7,
     shak_samvat: year - 77
   };
 }
