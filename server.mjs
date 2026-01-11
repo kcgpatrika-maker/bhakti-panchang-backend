eeb9de6e6a1f94f5aaf3eadda85b01b2e92f21df
kcgpatrika-maker <kcg.patrika@gmail.com>
2026-01-11 21:26:14 +0530

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -1,6 +1,6 @@
-const express = require("express");
-const cors = require("cors");
-const cheerio = require("cheerio");
+import express from "express";
+import cors from "cors";
+import cheerio from "cheerio";
 
 const app = express();
 app.use(cors());
