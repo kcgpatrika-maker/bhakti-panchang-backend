9d1c8567e86a5c0032bee28d73612e37fe0812ac
kcgpatrika-maker <kcg.patrika@gmail.com>
2026-02-12 17:13:56 +0530

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -16,7 +16,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use("/", express.static(path.join(__dirname, "public")));
 
 const PORT = process.env.PORT || 3000;
 const URL = "https://www.srimandir.com/hi/panchang";
